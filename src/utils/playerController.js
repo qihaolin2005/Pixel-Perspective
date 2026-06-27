0acef2c1c79ea2bf6bcc3ca67b7a7b940e5a7d6b
Qihao Lin <qihaolin2005@gmail.com>
2026-06-27 16:17:22 -0500

    added animations

--- src/utils/playerController.js
+++ src/utils/playerController.js
@@ -4,6 +4,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y, texture) {
         super(scene, x, y, texture);
         this.scene = scene;
+        this.createAnimation();
     }
 
     addToScene() {
@@ -18,22 +19,83 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     update() {
         const speed = 150;
         this.setVelocity(0);
-        if (this.cursors.left.isDown && this.cursors.up.isDown) {
-            this.setVelocityX(-(2/3 * speed));
-            this.setVelocityY(-(1/3 * speed));
+
+        let vx = 0;
+        let vy = 0;
+
+        let dir = 'none';
+
+        if (this.cursors.up.isDown && this.cursors.left.isDown) {
+            vx = -2/3 * speed;
+            vy = -1/3 * speed;
+            dir = 'northeast';
+        }
+        else if (this.cursors.up.isDown && this.cursors.right.isDown) {
+            vx = 2/3 * speed;
+            vy = -1/3 * speed;
+            dir = 'northwest';
+        }
+        else if (this.cursors.down.isDown && this.cursors.left.isDown) {
+            vx = -2/3 * speed;
+            vy = 1/3 * speed;
+            dir = 'southeast';
+        }
+        else if (this.cursors.down.isDown && this.cursors.right.isDown) {
+            vx = 2/3 * speed;
+            vy = 1/3 * speed;
+            dir = 'southwest';
         }
         else if (this.cursors.left.isDown) {
-            this.setVelocityX(-speed);
+            vx = -speed;
+            dir = 'east';
         }
         else if (this.cursors.right.isDown) {
-            this.setVelocityX(speed);
+            vx = speed;
+            dir = 'west';
         }
         else if (this.cursors.up.isDown) {
-            this.setVelocityY(-speed);
+            vy = -speed;
+            dir = 'north';
         }
         else if (this.cursors.down.isDown) {
-            this.setVelocityY(speed);
+            vy = speed;
+            dir = 'south';
         }
+
+        if (vx !== 0 || vy !== 0) {
+            this.setVelocityX(vx);
+            this.setVelocityY(vy);
+
+            this.anims.play(dir, true);
+        }
+        else {
+            this.anims.stop();
+        }
+
+
+    }
+
+    createAnimation() {
+
+        const directions = 
+        ['east', 'west', 'south', 'north', 'southeast', 'southwest', 'northeast', 'northwest'];
+
+        let current = 0;
+
+        directions.forEach(direction => {
+            this.anims.create({
+                key: direction,
+                frames: this.anims.generateFrameNumbers('player_walking', {
+                    start: current,
+                    end: current + 3,
+                }),
+                frameRate: 10,
+                repeat: -1
+
+            });
+            current += 4
+        })
+
     }
 
 
